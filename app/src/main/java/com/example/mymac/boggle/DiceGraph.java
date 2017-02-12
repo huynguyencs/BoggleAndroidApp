166cd08f9dd6acbea206a85983a473eaaf13dfea
Brett <chafin@pdx.edu>
2017-02-11 18:18:12 -0800

    (Incomplete) Began work on new Graphing Algorithm

--- app/src/main/java/com/example/mymac/boggle/DiceGraph.java
+++ app/src/main/java/com/example/mymac/boggle/DiceGraph.java
@@ -1,7 +1,9 @@
 package com.example.mymac.boggle;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Created by brettchafin on 2/9/17.
@@ -12,40 +14,85 @@ import java.util.List;
 public class DiceGraph {
 
     static final int BOARD_SIZE = 16;
-    List<Edge> adjList[];
+    List<String> adjList[] = new ArrayList[16];
 
-    DiceGraph(Die[][] dice){
-        adjList = new LinkedList[BOARD_SIZE];
+    DiceGraph() {
         for(int i = 0; i < adjList.length; i++){
-            adjList[i] = new LinkedList<Edge>();
+            adjList[i] = new LinkedList<String>();
         }
 
-        /*
-        //creates
-        for(int i = 0; i < 4; i++){
-            for (int j = 0; i < 4; j++){
-                Edge toAdd = new Edge(dice[i][j].boardPosition);
-                adjList[dice[i][j].boardPosition].add(toAdd);
-            }
-
-        }*/
+        createGraph();
 
     }
 
-
-
-
-
-    public void addAdjacentEdges(){
-
+    private void addEdge(String source, String destination) {
+        adjList[Integer.getInteger(source) - 1].add(destination);
     }
 
-    public class Edge {
+    private ListIterator<String> adjacentTo(String node) {
 
-        int die;
+        ListIterator<String> listIterator = adjList[Integer.getInteger(node)].listIterator();
 
-        Edge(int die){
-             this.die = die;
+        return listIterator;
         }
+
+    private void createGraph(){
+        addEdge("1", "2");
+        addEdge("1", "5");
+        addEdge("1", "6");
+
+        addEdge("2", "1");
+        addEdge("2", "3");
+        addEdge("2", "5");
+        addEdge("2", "6");
+        addEdge("2", "7");
+
+        addEdge("3", "2");
+        addEdge("3", "6");
+        addEdge("3", "7");
+        addEdge("3", "8");
+        addEdge("3", "4");
+
+        addEdge("4", "3");
+        addEdge("4", "7");
+        addEdge("4", "8");
+
+        addEdge("5", "1");
+        addEdge("5", "2");
+        addEdge("5", "6");
+        addEdge("5", "10");
+        addEdge("5", "9");
+
+        addEdge("6", "2");
+        addEdge("6", "1");
+        addEdge("6", "5");
+        addEdge("6", "9");
+        addEdge("6", "10");
+        addEdge("6", "11");
+        addEdge("6", "7");
+        addEdge("6", "3");
+
+        addEdge("7", "2");
+        addEdge("7", "6");
+        addEdge("7", "10");
+        addEdge("7", "11");
+        addEdge("7", "12");
+        addEdge("7", "8");
+        addEdge("7", "4");
+        addEdge("7", "3");
+
+        addEdge("8", "4");
+        addEdge("8", "3");
+        addEdge("8", "7");
+        addEdge("8", "11");
+        addEdge("8", "12");
+
+        addEdge("9", "5");
+        addEdge("9", "6");
+        addEdge("9", "10");
+        addEdge("9", "14");
+        addEdge("9", "13");
+
     }
+
 }
