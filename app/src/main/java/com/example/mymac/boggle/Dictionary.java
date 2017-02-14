ab7e986cbc831d60f49fc47ec8d5ad9c5481bc1b
huynguyencs <huy8@pdx.edu>
2017-02-14 00:32:22 -0800

    Finished implementing dictionary. Working on checking validity of candidate words

--- app/src/main/java/com/example/mymac/boggle/Dictionary.java
+++ app/src/main/java/com/example/mymac/boggle/Dictionary.java
@@ -1,31 +1,85 @@
 package com.example.mymac.boggle;
 
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 /**
  * Created by brettchafin on 2/9/17.
  */
 
-public class Dictionary {
-
-    Hashtable wordDictionary;
+public class Dictionary extends AppCompatActivity {
+    Hashtable <Integer, ArrayList<String>> wordDictionary;
 
     //constructor
-    Dictionary() {
-        createWordDictionary();
+    Dictionary(InputStream in) {//K-V
+        wordDictionary = new Hashtable <Integer, ArrayList<String>> ();
+        createWordDictionary(in);
     }
 
-    private boolean createWordDictionary() {
+    private boolean createWordDictionary(InputStream inputS) {
+        BufferedReader textReader = new BufferedReader(new InputStreamReader(inputS));
+        //StringBuilder sb = new StringBuilder();
+        String line = null;
+        try {
+            while ((line = textReader.readLine()) != null) {
+                ArrayList <String> tempList = null;
+                int key = generateHashKey(line);
 
+                if (wordDictionary.containsKey(key)) {
+                    tempList = wordDictionary.get(key);
+                    if(tempList == null)
+                        tempList = new ArrayList<String>();
+                    tempList.add(line);
+                } else {
+                    tempList = new ArrayList<String>();
+                    tempList.add(line);
+                }
+                wordDictionary.put(key,tempList);
+            }
+            inputS.close();
+            //sb.toString();
+        }catch (Exception e) { }
         return true;
     }
 
     public String[] findValidWords(String[] possibleString) {
-        return null;
+        ArrayList<String> possibleWords = new ArrayList<String>();
+        for(int i = 0; i < possibleString.length; ++i){
+            if(isValid(possibleString[i]))
+                possibleWords.add(possibleString[i]);
+        }
+        return possibleWords.toArray(new String[0]);
     }
 
-    private boolean isValid(String word) {
-        return true;
+    public boolean isValid(String word) {
+        int key = generateHashKey(word);
+        if(wordDictionary.containsKey(key)){
+            ArrayList <String> tempList = wordDictionary.get(key);
+            if(tempList.contains(word))
+                return true;
+        }
+        return false;
     }
 
+    private int generateHashKey(String a_word){
+        if (a_word.length() >= 3 && a_word.length() <= 4){
+            return 1;
+        }
+        else if (a_word.length() == 5){
+            return 2;
+        }
+        else if (a_word.length() == 6){
+            return 3;
+        }
+        else if (a_word.length() == 7){
+            return 5;
+        }
+        return 10;
+
+    }
 }
--- app/src/main/java/com/example/mymac/boggle/MainBoard.java
+++ app/src/main/java/com/example/mymac/boggle/MainBoard.java
@@ -3,30 +3,37 @@ package com.example.mymac.boggle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.InputStream;
 import java.util.Random;
 
+import static java.lang.System.in;
 
-public class MainBoard extends AppCompatActivity {
 
+public class MainBoard extends AppCompatActivity implements View.OnClickListener{
     public TextView timerText;
     public TextView user_score;
     private CountDownTimer timer;
 
 
-    public Die[][] Dice;
+    public Die[] Dice;
     private String[] possibleWords;
     private String[] wordsFound;
     private Dictionary dictionary;
-
+    private StringBuilder selectingWord = new StringBuilder();
+    private boolean [] value = new boolean [16];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_board);
-
-        dictionary = new Dictionary();
+        try {
+            InputStream inputS = getResources().openRawResource(R.raw.dictionary);
+            dictionary = new Dictionary(inputS);
+        } catch (Exception e) { }
 
         //fully create a new Board
         newBoard();
@@ -40,7 +47,23 @@ public class MainBoard extends AppCompatActivity {
 
         user_score = (TextView) this.findViewById(R.id.score);
         user_score.setText("Your score: " + 0);
+    }
 
+    @Override
+    public void onClick(View v) {
+        // default method for handling onClick Events..
+        switch (v.getId()) {
+            case R.id.submit:
+                System.out.println("Checking user's words to dictionary.");
+                break;
+
+            default:
+                //array button id 0-15
+
+                selectingWord.append(((Button)v).getText().toString());
+                System.out.println(selectingWord);
+                break;
+        }
 
     }
 
@@ -50,7 +73,7 @@ public class MainBoard extends AppCompatActivity {
             wordsFound = new String[0];
             randomDice();
             String[] wordPossiblilities = possiblePaths();
-            if (validateBoard(wordPossiblilities)) continue;
+            //if (validateBoard(wordPossiblilities)) continue;
             return true;
         }
     }
@@ -76,11 +99,9 @@ public class MainBoard extends AppCompatActivity {
 
     //creates a new 2d array of Die objects
     private boolean randomDice(){
-        Dice = new Die[4][4];
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 4; j++) {
-                Dice[i][j] = new Die();
-            }
+        Dice = new Die[16];
+        for(int i = 0; i < 16; i++) {
+            Dice[i] = new Die();
         }
         return true;
     }
@@ -111,52 +132,71 @@ public class MainBoard extends AppCompatActivity {
 
     private void buttonCreation() {
         Button p1_button = (Button)findViewById(R.id.button1);
-        p1_button.setText(Character.toString(Dice[0][0].topLetter));
+        p1_button.setText(Character.toString(Dice[0].topLetter));
+        p1_button.setOnClickListener(this);
 
         Button p2_button = (Button)findViewById(R.id.button2);
-        p2_button.setText(Character.toString(Dice[0][1].topLetter));
+        p2_button.setText(Character.toString(Dice[1].topLetter));
+        p2_button.setOnClickListener(this);
 
         Button p3_button = (Button)findViewById(R.id.button3);
-        p3_button.setText(Character.toString(Dice[0][2].topLetter));
+        p3_button.setText(Character.toString(Dice[2].topLetter));
+        p3_button.setOnClickListener(this);
 
         Button p4_button = (Button)findViewById(R.id.button4);
-        p4_button.setText(Character.toString(Dice[0][3].topLetter));
+        p4_button.setText(Character.toString(Dice[3].topLetter));
+        p4_button.setOnClickListener(this);
 
         Button p5_button = (Button)findViewById(R.id.button5);
-        p5_button.setText(Character.toString(Dice[1][0].topLetter));
+        p5_button.setText(Character.toString(Dice[4].topLetter));
+        p5_button.setOnClickListener(this);
 
         Button p6_button = (Button)findViewById(R.id.button6);
-        p6_button.setText(Character.toString(Dice[1][1].topLetter));
+        p6_button.setText(Character.toString(Dice[5].topLetter));
+        p6_button.setOnClickListener(this);
 
         Button p7_button = (Button)findViewById(R.id.button7);
-        p7_button.setText(Character.toString(Dice[1][2].topLetter));
+        p7_button.setText(Character.toString(Dice[6].topLetter));
+        p7_button.setOnClickListener(this);
 
         Button p8_button = (Button)findViewById(R.id.button8);
-        p8_button.setText(Character.toString(Dice[1][3].topLetter));
+        p8_button.setText(Character.toString(Dice[7].topLetter));
+        p8_button.setOnClickListener(this);
 
         Button p9_button = (Button)findViewById(R.id.button9);
-        p9_button.setText(Character.toString(Dice[2][0].topLetter));
+        p9_button.setText(Character.toString(Dice[8].topLetter));
+        p9_button.setOnClickListener(this);
 
         Button p10_button = (Button)findViewById(R.id.button10);
-        p10_button.setText(Character.toString(Dice[2][1].topLetter));
+        p10_button.setText(Character.toString(Dice[9].topLetter));
+        p10_button.setOnClickListener(this);
 
         Button p11_button = (Button)findViewById(R.id.button11);
-        p11_button.setText(Character.toString(Dice[2][2].topLetter));
+        p11_button.setText(Character.toString(Dice[10].topLetter));
+        p11_button.setOnClickListener(this);
 
         Button p12_button = (Button)findViewById(R.id.button12);
-        p12_button.setText(Character.toString(Dice[2][3].topLetter));
+        p12_button.setText(Character.toString(Dice[11].topLetter));
+        p12_button.setOnClickListener(this);
 
         Button p13_button = (Button)findViewById(R.id.button13);
-        p13_button.setText(Character.toString(Dice[3][0].topLetter));
+        p13_button.setText(Character.toString(Dice[12].topLetter));
+        p13_button.setOnClickListener(this);
 
         Button p14_button = (Button)findViewById(R.id.button14);
-        p14_button.setText(Character.toString(Dice[3][1].topLetter));
+        p14_button.setText(Character.toString(Dice[13].topLetter));
+        p14_button.setOnClickListener(this);
 
         Button p15_button = (Button)findViewById(R.id.button15);
-        p15_button.setText(Character.toString(Dice[3][2].topLetter));
+        p15_button.setText(Character.toString(Dice[14].topLetter));
+        p15_button.setOnClickListener(this);
 
         Button p16_button = (Button)findViewById(R.id.button16);
-        p16_button.setText(Character.toString(Dice[3][3].topLetter));
+        p16_button.setText(Character.toString(Dice[15].topLetter));
+        p16_button.setOnClickListener(this);
+
+        Button submitBtn = (Button)findViewById(R.id.submit);
+        submitBtn.setOnClickListener(this);
     }
 }
 
