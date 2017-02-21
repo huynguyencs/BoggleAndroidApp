b8b214646f89695e24b33f6af34dd6d8bdb2e44a
huynguyencs <huy8@pdx.edu>
2017-02-21 13:20:48 -0800

    Sliding Finger to select characters completed.

--- app/src/main/java/com/example/mymac/boggle/MainBoard.java
+++ app/src/main/java/com/example/mymac/boggle/MainBoard.java
@@ -1,9 +1,17 @@
 package com.example.mymac.boggle;
 
+import android.annotation.TargetApi;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
+import android.os.Build;
+import android.util.DisplayMetrics;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 import java.util.Arrays;
 import android.content.Intent;
@@ -26,6 +34,7 @@ import java.util.Random;
 import java.util.Scanner;
 import java.io.File;
 
+import static com.example.mymac.boggle.R.id.button1;
 import static java.lang.System.in;
 
 public class MainBoard extends AppCompatActivity implements View.OnTouchListener{
@@ -42,12 +51,20 @@ public class MainBoard extends AppCompatActivity implements View.OnTouchListener
 
     private int userScore;
     private boolean[] flag = new boolean[16];
+    private int [] BtnIds = {R.id.button1, R.id.button2, R.id.button3, R.id.button4,
+                             R.id.button5, R.id.button6, R.id.button7, R.id.button8,
+                             R.id.button9, R.id.button10, R.id.button11, R.id.button12,
+                             R.id.button13, R.id.button14, R.id.button15, R.id.button16};
     private GestureDetector gestureDetector;
 
     int prevRow = 0;
     int prevCol = 0;
     int curRow;
     int curCol;
+    private Point[] BtnLocation;
+    RelativeLayout mainScreen;
+    Button object;
+    int offsetX, offsetY;
 
     Button p1_button; Button p2_button; Button p3_button; Button p4_button; Button p5_button; Button p6_button;
     Button p7_button; Button p8_button; Button p9_button; Button p10_button; Button p11_button; Button p12_button;
@@ -66,7 +83,6 @@ public class MainBoard extends AppCompatActivity implements View.OnTouchListener
 
 
         //fully create a new Board
-        gestureDetector = new GestureDetector(this, new SingleTapConfirm());
         try {
             newBoard();
         } catch (IOException e) {
@@ -78,6 +94,24 @@ public class MainBoard extends AppCompatActivity implements View.OnTouchListener
         resetBtnBackground();
         //get possibleWords from dictionary
 
+        //get location from buttons
+        BtnLocation = new Point [16];
+        mainScreen = (RelativeLayout) findViewById(R.id.activity_main_board);
+
+        mainScreen.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        // Layout has happened here.
+                        readLocation();
+                        // Don't forget to remove your listener when you are done with it.
+                        if (Build.VERSION.SDK_INT<16) {
+                            removeLayoutListenerPre16(mainScreen.getViewTreeObserver(),this);
+                        } else {
+                            removeLayoutListenerPost16(mainScreen.getViewTreeObserver(), this);
+                        }
+                    }
+                });
 
         timerText = (TextView) this.findViewById(R.id.timer);
         timer = new countDownTimer(60 * 1000, 1 * 1000);
@@ -189,7 +223,7 @@ public class MainBoard extends AppCompatActivity implements View.OnTouchListener
     }
 
     private void buttonCreation() {
-        p1_button = (Button)findViewById(R.id.button1);
+        p1_button = (Button)findViewById(button1);
         p1_button.setText((dice[0].topLetter));
         p1_button.setOnTouchListener(this);
         p2_button = (Button)findViewById(R.id.button2);
@@ -274,10 +308,195 @@ public class MainBoard extends AppCompatActivity implements View.OnTouchListener
     }
 
     @Override
-    public boolean onTouch(View v, MotionEvent arg1) {
-        // default method for handling onClick Events.
+    public boolean onTouch(View v, MotionEvent event) {
+            //TODO: Finger Sliding
+            int dieNo;
+            switch(event.getAction()) {
+                case MotionEvent.ACTION_DOWN:   // start here
+                    dieNo = trackLocation((int)event.getRawX(),(int)event.getRawY());
+                    if(dieNo != -1)
+                        ButtonHandler(dieNo);
+                    if(v.getId() == R.id.submit){
+                        System.out.println("Checking user's words to dictionary.");
+                        //if user's submitted word is valid
+                        //if(Arrays.asList(possibleWords).contains(selectingWord.toString())){
+                        if(dictionary.isValid(selectingWord.toString())){
+                            //check if it's already in the list of found word
+                            if(wordsFound.contains(selectingWord.toString())){
+                                Toast.makeText(getApplicationContext(), "THIS WORD HAS ALREADY BEEN SUBMITTED!!!", Toast.LENGTH_SHORT).show();
+                                resetBtnBackground();
+                            }
+                            else{
+                                int pts = 0;
+                                wordsFound.add(selectingWord.toString());
+                                if (selectingWord.length() >= 3 && selectingWord.length() <= 4){
+                                    pts = 1;
+                                }else if (selectingWord.length() == 5){
+                                    pts = 2;
+                                }else if (selectingWord.length() == 6){
+                                    pts = 3;
+                                }else if (selectingWord.length() == 7){
+                                    pts = 5;
+                                }else if (selectingWord.length() >= 8) {
+                                    pts = 10;
+                                }
+                                userScore += pts;
+                                CharSequence text = "YOU EARNED " + pts + " POINTS!!!";
+                                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                                updateTextView();
+                                resetBtnBackground();
+                            }
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "INVALID WORD!!!", Toast.LENGTH_SHORT).show();
+                            resetBtnBackground();
+                        }
+                        selectingWord.delete(0, selectingWord.length());
+                        flag = new boolean[16];
+                    }
+                    break;
 
-        if (gestureDetector.onTouchEvent(arg1)) {
+
+                case MotionEvent.ACTION_MOVE:
+                    dieNo = trackLocation((int)event.getRawX(),(int)event.getRawY());
+                    if(dieNo != -1)
+                        ButtonHandler(dieNo);
+                    break;
+            }
+
+
+
+        return true;
+    }
+
+    private void ButtonHandler(int dieNumber) {
+        curRow = dieNumber/4;
+        curCol = dieNumber%4;
+        View v = (View) findViewById(BtnIds[dieNumber]);
+
+        if (flag[dieNumber] == true) {
+            for(int i = 0; i <16; ++i){
+                System.out.print(flag[i] + " ");
+            }
+            selectingWord.delete(0, selectingWord.length());
+            flag = new boolean[16];
+            resetBtnBackground();
+            Toast.makeText(getApplicationContext(), "THIS BUTTON HAS ALREADY BEEN PRESSED!!!", Toast.LENGTH_SHORT).show();
+
+        } else {
+            if (selectingWord.length() == 0) {
+                prevRow = curRow;
+                prevCol = curCol;
+                selectingWord.append(((Button)v).getText().toString());
+                flag[dieNumber] = true;
+                v.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
+            } else {
+                if (curRow - prevRow <= 1 && curRow - prevRow >= -1
+                        && curCol - prevCol <= 1 && curCol - prevCol >= -1) {
+                    selectingWord.append(((Button)v).getText().toString());
+                    flag[dieNumber] = true;
+                    prevRow = curRow;
+                    prevCol = curCol;
+                    v.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
+                } else {
+                    selectingWord.delete(0, selectingWord.length());
+                    flag = new boolean[16];
+                    resetBtnBackground();
+                    Toast.makeText(getApplicationContext(), "TWO BUTTONS MUST BE ADJACENT!!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        System.out.println(selectingWord);
+    }
+
+    private int trackLocation(int x, int y){
+        //System.out.println("X: " + x);
+        //System.out.println("Y: " + y);
+        for(int i = 0; i < 16; ++i){
+            if (x > (BtnLocation[i].x + offsetX/4) && x < (BtnLocation[i].x + offsetX* 3/4) &&
+                    y > (BtnLocation[i].y + offsetY/4) && y < (BtnLocation[i].y + offsetY * 3/4)){
+                    //System.out.println("true");
+                    //System.out.println("i: " + i);
+                    if(!flag[i]) {
+                        return i;
+                    }
+                }
+        }
+        return -1;
+    }
+
+    private void readLocation(){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        //offsetX = displayMetrics.widthPixels - mainScreen.getMeasuredWidth();
+        //offsetY = displayMetrics.heightPixels - mainScreen.getMeasuredHeight();
+        offsetX = p1_button.getWidth();
+        offsetY = p1_button.getHeight();
+        int[] locationInWindow = new int[2];
+
+        p1_button.getLocationInWindow(locationInWindow);
+        BtnLocation[0] = new Point(locationInWindow[0], locationInWindow[1]);
+        p2_button.getLocationInWindow(locationInWindow);
+        BtnLocation[1] = new Point(locationInWindow[0], locationInWindow[1]);
+        p3_button.getLocationInWindow(locationInWindow);
+        BtnLocation[2] = new Point(locationInWindow[0], locationInWindow[1]);
+        p4_button.getLocationInWindow(locationInWindow);
+        BtnLocation[3] = new Point(locationInWindow[0], locationInWindow[1]);
+        p5_button.getLocationInWindow(locationInWindow);
+        BtnLocation[4] = new Point(locationInWindow[0], locationInWindow[1]);
+        p6_button.getLocationInWindow(locationInWindow);
+        BtnLocation[5] = new Point(locationInWindow[0], locationInWindow[1]);
+        p7_button.getLocationInWindow(locationInWindow);
+        BtnLocation[6] = new Point(locationInWindow[0], locationInWindow[1]);
+        p8_button.getLocationInWindow(locationInWindow);
+        BtnLocation[7] = new Point(locationInWindow[0], locationInWindow[1]);
+        p9_button.getLocationInWindow(locationInWindow);
+        BtnLocation[8] = new Point(locationInWindow[0], locationInWindow[1]);
+        p10_button.getLocationInWindow(locationInWindow);
+        BtnLocation[9] = new Point(locationInWindow[0], locationInWindow[1]);
+        p11_button.getLocationInWindow(locationInWindow);
+        BtnLocation[10] = new Point(locationInWindow[0], locationInWindow[1]);
+        p12_button.getLocationInWindow(locationInWindow);
+        BtnLocation[11] = new Point(locationInWindow[0], locationInWindow[1]);
+        p13_button.getLocationInWindow(locationInWindow);
+        BtnLocation[12] = new Point(locationInWindow[0], locationInWindow[1]);
+        p14_button.getLocationInWindow(locationInWindow);
+        BtnLocation[13] = new Point(locationInWindow[0], locationInWindow[1]);
+        p15_button.getLocationInWindow(locationInWindow);
+        BtnLocation[14] = new Point(locationInWindow[0], locationInWindow[1]);
+        p16_button.getLocationInWindow(locationInWindow);
+        BtnLocation[15] = new Point(locationInWindow[0], locationInWindow[1]);
+
+
+        //int[] locationOnScreen = new int[2];
+        //object.getLocationOnScreen(locationOnScreen);
+        for(int i = 0; i < 16; ++i) {
+            System.out.println(
+                    "\n"
+                            + "getLocationInWindow() - " + BtnLocation[i].x + " : " + BtnLocation[i].y + "\n"
+                            //+ "getLocationOnScreen() - " + locationOnScreen[0] + " : " + locationOnScreen[1] + "\n"
+                            + "Offset x: y - " + offsetX + " : " + offsetY);
+        }
+
+    }
+
+    @SuppressWarnings("deprecation")
+    private void removeLayoutListenerPre16(ViewTreeObserver observer, ViewTreeObserver.OnGlobalLayoutListener listener){
+        observer.removeGlobalOnLayoutListener(listener);
+    }
+
+    @TargetApi(16)
+    private void removeLayoutListenerPost16(ViewTreeObserver observer, ViewTreeObserver.OnGlobalLayoutListener listener){
+        observer.removeOnGlobalLayoutListener(listener);
+    }
+}
+
+
+
+/*
+FOR BACKUP
+if (gestureDetector.onTouchEvent(event)) {
+            System.out.println("tap");
             switch (v.getId()) {
                 case R.id.submit:
                     System.out.println("Checking user's words to dictionary.");
@@ -319,108 +538,80 @@ public class MainBoard extends AppCompatActivity implements View.OnTouchListener
                     break;
 
                 case R.id.button1:
-                    ButtonHandler(v, 0);
+                    ButtonHandler(0);
                     break;
                 case R.id.button2:
-                    ButtonHandler(v, 1);
+                    ButtonHandler(1);
                     break;
                 case R.id.button3:
-                    ButtonHandler(v, 2);
+                    ButtonHandler(2);
                     break;
                 case R.id.button4:
-                    ButtonHandler(v, 3);
+                    ButtonHandler(3);
                     break;
                 case R.id.button5:
-                    ButtonHandler(v, 4);
+                    ButtonHandler(4);
                     break;
                 case R.id.button6:
-                    ButtonHandler(v, 5);
+                    ButtonHandler(5);
                     break;
                 case R.id.button7:
-                    ButtonHandler(v, 6);
+                    ButtonHandler(6);
                     break;
                 case R.id.button8:
-                    ButtonHandler(v, 7);
+                    ButtonHandler(7);
                     break;
                 case R.id.button9:
-                    ButtonHandler(v, 8);
+                    ButtonHandler(8);
                     break;
                 case R.id.button10:
-                    ButtonHandler(v, 9);
+                    ButtonHandler(9);
                     break;
                 case R.id.button11:
-                    ButtonHandler(v, 10);
+                    ButtonHandler(10);
                     break;
                 case R.id.button12:
-                    ButtonHandler(v, 11);
+                    ButtonHandler(11);
                     break;
                 case R.id.button13:
-                    ButtonHandler(v, 12);
+                    ButtonHandler(12);
                     break;
                 case R.id.button14:
-                    ButtonHandler(v, 13);
+                    ButtonHandler(13);
                     break;
                 case R.id.button15:
-                    ButtonHandler(v, 14);
+                    ButtonHandler(14);
                     break;
                 case R.id.button16:
-                    ButtonHandler(v, 15);
+                    ButtonHandler(15);
                     break;
                 default:
                     break;
             }
             System.out.println(selectingWord);
             return false;
-        } else {
-            //TODO: Finger Sliding
         }
 
-        return false;
-    }
-
-    private void ButtonHandler(View v, int dieNumber) {
-        curRow = dieNumber/4;
-        curCol = dieNumber%4;
-        if (flag[dieNumber] == true) {
-            selectingWord.delete(0, selectingWord.length());
-            flag = new boolean[16];
-            resetBtnBackground();
-            Toast.makeText(getApplicationContext(), "THIS BUTTON HAS ALREADY BEEN PRESSED!!!", Toast.LENGTH_SHORT).show();
-        } else {
-            if (selectingWord.length() == 0) {
-                prevRow = curRow;
-                prevCol = curCol;
-                selectingWord.append(((Button)v).getText().toString());
-                flag[dieNumber] = true;
-                v.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-            } else {
-                System.out.println(prevCol + " " + prevRow + " " + curRow + " " + curCol);
-                if (curRow - prevRow <= 1 && curRow - prevRow >= -1
-                        && curCol - prevCol <= 1 && curCol - prevCol >= -1) {
-                    selectingWord.append(((Button)v).getText().toString());
-                    flag[dieNumber] = true;
-                    prevRow = curRow;
-                    prevCol = curCol;
-                    v.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-                } else {
-                    selectingWord.delete(0, selectingWord.length());
-                    flag = new boolean[16];
-                    resetBtnBackground();
-                    Toast.makeText(getApplicationContext(), "TWO BUTTONS MUST BE ADJACENT!!!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-
-    private class SingleTapConfirm extends GestureDetector.SimpleOnGestureListener {
+        private class SingleTapConfirm extends GestureDetector.SimpleOnGestureListener {
 
         @Override
         public boolean onSingleTapUp(MotionEvent event) {
             return true;
         }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            //do something
+            return true;
+        }
+        @Override
+        public void onLongPress(MotionEvent e) {
+            super.onLongPress(e);
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            return super.onDoubleTap(e);
+        }
     }
-}
-
-
-
-
+ */
